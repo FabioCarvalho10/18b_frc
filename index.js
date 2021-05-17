5875b927740d934a7d2efb2f59c49fde4d30433c
FabioCarvalho <fabiocarvalho.br@hotmail.com>
2021-05-17 17:41:06 -0300

    Primeiro Commit Meu Sistema

---
+++ index.js
@@ -0,0 +1,9 @@
+$("#btnAdicionar").click(() => {
+    $("#tblDados tbody").append(`<tr>
+            <td>-- # --</td>
+            <td>Nome</td>
+            <td>-- sobrenome --</td>
+            <td>01/01/1980</td>
+        </tr>`)
+})
+
